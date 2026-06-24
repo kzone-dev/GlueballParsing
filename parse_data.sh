a142b45d628bbeff8da95af8d7e6d0f6232d337b
kzone-dev <nmrbrito2000@gmail.com>
2026-06-24 20:19:26 +0100

    no longer have split glueball data across several folders

--- parse_data.sh
+++ parse_data.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-RAWLOGS_DIR=$1
+RAWLOGS_PATH=$1
 OUTPUT_DIR=$2
 ENSEMBLE_NAME=$3
 RPC_STR=$4
 
-DATA_DIRS=$(ls -dv $RAWLOGS_DIR/run-*/out_0)
-
-echo "Glueball raw data DIR:" $1
+echo "Glueball raw data path:" $1
 echo "Output dir:" $2
 echo "Ensemble name:" $3
 
-for dir in $DATA_DIRS
-do
-    grep "$RPC_STR" $dir >> ${ENSEMBLE_NAME}_${RPC_STR}_result.out
-done
+grep "$RPC_STR" $RAWLOGS_PATH >> ${ENSEMBLE_NAME}_${RPC_STR}_result.out
 
 mv ${ENSEMBLE_NAME}_${RPC_STR}_result.out $OUTPUT_DIR
